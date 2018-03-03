d379079ad4d930ed8ed3c5bd8c1c825cd84f4b71
mkhuthir <mkhuthir@gmail.com>
2018-03-03 12:23:07 +0400

    simple

--- NeuralNetwork/simple.py
+++ NeuralNetwork/simple.py
@@ -6,11 +6,13 @@ import numpy as np
 def sigmoid(x):
     return 1/(1 + np.exp(-x))
 
-inputs = np.array([0.7, -0.3])
+# inputs,weights, and bias
+inputs  = np.array([0.7, -0.3])
 weights = np.array([0.1, 0.8])
 bias = -0.1
 
 # Calculate the output
 output = sigmoid(np.dot(weights, inputs) + bias)
 
-print('Output: {}'.format(output))
+print('\nSimple Neural network:\n\nOutput=Sigmoid(w1.x1 + w2.x2 + b)')
+print('\nx-inputs\t:{}\nw-weights\t:{}\nb-bias  \t:{}\n\nOutput  \t:{}\n'.format(inputs,weights,bias,output))
